5f85560e98295db637ed89c371ab858f73c837b7
Sharib Ansari <ansarisharib8445@gmail.com>
2026-06-22 23:27:24 +0530

    Cart section - generating html via js and rendering it into page :)

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,13 @@
 // modules hlp to prevent naming conflict and and we dont worry about the sequece of the file in main html file or else where
-export const cart = [];
+export const cart = [{
+  productId:
+  'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2,
+},{
+  productId:
+  '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
 
 //CREATING FUNCTION THAT ADD WORKING IN ADD TO CART BUTTON
 export function addToCart(productId){
