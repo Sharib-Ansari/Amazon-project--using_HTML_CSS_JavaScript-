c71194a4f5e040d99f6469363a607e1f65a0b058
Sharib Ansari <ansarisharib8445@gmail.com>
2026-06-25 20:50:42 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,23 @@
 // modules hlp to prevent naming conflict and and we dont worry about the sequece of the file in main html file or else where
-export let cart = [{
-  productId:
-  'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-},{
-  productId:
-  '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+
+export let cart = JSON.parse(localStorage.getItem('cart')); //this is used to change the item back to array 
+
+if(!cart) {
+    cart = [{
+    productId:
+    'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  },{
+    productId:
+    '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+// creating a function to store the data about cart products
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 //CREATING FUNCTION THAT ADD WORKING IN ADD TO CART BUTTON
 export function addToCart(productId){
@@ -34,6 +44,8 @@ export function addToCart(productId){
         quantity: quantity
       });
     }
+    //calling the function to save the cart products
+    saveToStorage();
  };
 
  //fucntion for remove the item from cart
@@ -47,6 +59,9 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;// update the cart
+
+  //calling the function to save the cart products
+  saveToStorage();
 
 
  }
