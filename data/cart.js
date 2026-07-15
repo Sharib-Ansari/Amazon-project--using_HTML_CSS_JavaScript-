c0e4bec93a9bd04b131dcf56062d56ff2a415052
Sharib Ansari <ansarisharib8445@gmail.com>
2026-07-15 20:48:48 +0530

    Create automated tests for addToCart :)

--- data/cart.js
+++ data/cart.js
@@ -1,19 +1,25 @@
 // modules hlp to prevent naming conflict and and we dont worry about the sequece of the file in main html file or else where
 
-export let cart = JSON.parse(localStorage.getItem('cart')); //this is used to change the item back to array 
+export let cart;
 
-if(!cart) {
-    cart = [{
-    productId:
-    'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  },{
-    productId:
-    '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart')); //this is used to change the item back to array 
+
+  if(!cart) {
+      cart = [{
+      productId:
+      'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    },{
+      productId:
+      '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
 }
 
 // creating a function to store the data about cart products
@@ -33,10 +39,15 @@ export function addToCart(productId){
     }); 
     
     //Adds that selected quantity to the cart instead of always adding 1 feature
+    let quantity = 1;
+
     const quantitySelector = document.querySelector(
       `.js-quantity-selector-${productId}`
     );
-    const quantity = Number(quantitySelector.value);
+
+    if (quantitySelector) {
+      quantity = Number(quantitySelector.value);
+    }
 
     if (matchingItem) {
     matchingItem.quantity += quantity;
