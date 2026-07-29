730a7726ce114b82f7dcb9e0268a5cc54c91ab7a
Sharib Ansari <ansarisharib8445@gmail.com>
2026-07-29 23:30:48 +0530

    Convert products into classes :)

--- data/products.js
+++ data/products.js
@@ -1,5 +1,5 @@
 // we use modle to use this file in amazon.js by writing (export) at the starting of the file okkkk
-
+  import {formatCurrency} from '../scripts/utils/money.js';  
   export function getProduct (productId){ 
 
     let matchingProduct;
@@ -11,6 +11,47 @@
 
       return matchingProduct;
   }
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){//this is called converting objects into class
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars*10}.png`;
+  }
+
+  getPrice(){
+    return `${formatCurrency(this.priceCents)}` 
+  }
+}
+
+const product1 = new Product({
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  });
+;
 
 export const products = [
   {
@@ -671,4 +712,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {//.map just modify the elements and create a new array of it 
+  return new Product (productDetails);
+});
